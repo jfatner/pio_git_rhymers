f7cb93c7a8e4880a223b9c46484b63bbc9248067
Janusz Fątner <255588@edu.p.lodz.pl>
2026-03-15 12:19:53 +0100

    3. Automatyczne formatowanie projektu

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -9,9 +9,7 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     }
 
     public void countIn(int in) {
-        if (!callCheck() && in > peekaboo())
-            totalRejected++;
-        else
-            super.countIn(in);
+        if (!callCheck() && in > peekaboo()) totalRejected++;
+        else super.countIn(in);
     }
 }
