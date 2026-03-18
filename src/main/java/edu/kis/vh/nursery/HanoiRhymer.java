9a7f1af57a5e82a94029c66d15975a58b11f746d
Janusz Fątner <255588@edu.p.lodz.pl>
2026-03-18 11:43:27 +0100

    9.-11. Hermetyzacja artybutiw klas

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    private int totalRejected = 0;
 
     public int reportRejected() {
         return totalRejected;
@@ -11,5 +11,13 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo()) totalRejected++;
         else super.countIn(in);
+    }
+
+    public int getTotalRejected() {
+        return totalRejected;
+    }
+
+    public void setTotalRejected(int totalRejected) {
+        this.totalRejected = totalRejected;
     }
 }
